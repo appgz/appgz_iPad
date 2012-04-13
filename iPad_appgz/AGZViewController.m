2c5d03ec9d166c99ee0f21982cb8db9d0bdb9243
appgz <tarcake@appgz.com>
2012-04-13 15:45:06 +0800

    jhju

--- iPad_appgz/AGZViewController.m
+++ iPad_appgz/AGZViewController.m
@@ -48,7 +48,10 @@
                          [UIImage imageNamed:@"1-11.jpg"],
                          [UIImage imageNamed:@"1-12.jpg"],
                          [UIImage imageNamed:@"1-13.jpg"],
-                      
+                         [UIImage imageNamed:@"1-14.jpg"],
+                         [UIImage imageNamed:@"1-15.jpg"],
+               [UIImage imageNamed:@"1-14.jpg"],
+               [UIImage imageNamed:@"1-15.jpg"],
                [UIImage imageNamed:@"1-11.jpg"],
                          nil];
     
